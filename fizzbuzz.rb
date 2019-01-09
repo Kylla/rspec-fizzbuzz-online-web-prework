1b8235bcf90a85845ff39399d96bf766b29d87ab
Kylla Harris <kylla.harris@gmail.com>
2019-01-09 03:28:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def fizzbuzz(int)
   if int % 3 == 0 
   "Fizz"
@@ -8,6 +10,7 @@ def fizzbuzz(int)
   elsif int % 3 && int % 5 == 0
   "FizzBuzz"
     
+    binding.pry
   else 
     
   end
